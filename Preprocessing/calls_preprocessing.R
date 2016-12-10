968273ce4dabb21cfa625aa0b7507febf6bb4aea
Patrick Staples <ctphoenix@gmail.com>
2016-12-09 19:49:30 -0500

    Update and rename calls_preprocessing to calls_preprocessing.R

--- Preprocessing/calls_preprocessing.R
+++ Preprocessing/calls_preprocessing.R
@@ -1,4 +1,6 @@
 calls_preprocessing = function(){
+  # input: none
+  # output: matrix consisting of the calls for all patients and UNIX time stamps in seconds. (this standard should probably be changed.)
   if(file.exists("calls")){
     callmat = c()
     call_files = list.files("calls")
