66d29490a5a74bcade6eaed5a2d2a49e99e2c97d
Patrick Staples <ctphoenix@gmail.com>
2016-12-09 19:41:13 -0500

    Update and rename Output/LocationPlots/warps.R to Processing/warps.R

--- Processing/warps.R
+++ Processing/warps.R
@@ -1,4 +1,10 @@
 warps = function(points, warp_matrix, pushes, sd1=1, sd2=1){
+  # input:  points: 2xN matrix of two-dimensional points   
+  #         warp_matrix: 2xK matrix of two-dimensional locations that warp `points`.
+  #         pushes: vector of integers, 1 iff warp_matrix row value pushes points.  (pulls otherwise)
+  #         sd1: breadth of the warp in dimension 1
+  #         sd2: breadth of the warp in dimension 2
+  # output: 2xN matrix of warped two-dimensional points   
   total_diffs = points*0
   if(is.vector(warp_matrix)) warp_matrix = t(warp_matrix)
   for(i in 1:nrow(warp_matrix)){
